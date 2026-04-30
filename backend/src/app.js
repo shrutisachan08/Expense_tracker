302304ac00d1471ae7e7bee870791103ec19817a
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-04-30 23:50:59 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
   res.send("Expense Tracker API running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
