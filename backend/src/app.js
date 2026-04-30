333aac0067577ba70c2367ce9a0fa19ec0743161
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-04-30 21:42:14 +0530

    main source file done

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./db");
+
+const expenseRoutes = require("./routes/expenses");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/expenses", expenseRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API running");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
